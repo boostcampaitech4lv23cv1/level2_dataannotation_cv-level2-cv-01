341cbe921185f450d2d5df2fefc52989138f6cbd
MyeongheonChoi <audgjs99@gmail.com>
2022-12-08 06:19:03 +0000

    feat: add metric for valid set and modify gitignore

--- dataset.py
+++ dataset.py
@@ -392,7 +392,7 @@ class SceneTextDataset(Dataset):
         if self.color_jitter:
             funcs.append(A.ColorJitter(0.5, 0.5, 0.5, 0.25))
         if self.normalize:
-            funcs.append(A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
+            funcs.append(A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
         transform = A.Compose(funcs)
 
         image = transform(image=image)['image']
